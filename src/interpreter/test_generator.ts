08f26f50a66b7f02a51f5735316fe30bbddc9f52
Till Rohrmann <trohrmann@apache.org>
2024-10-17 17:19:15 +0200

    More changes

--- src/interpreter/test_generator.ts
+++ src/interpreter/test_generator.ts
@@ -31,7 +31,7 @@ const L0 = distribution([
   [
     CommandType.INCREMENT_STATE_COUNTER_INDIRECTLY,
     CommandType.CALL_SERVICE,
-    // CommandType.CALL_NEXT_LAYER_OBJECT,
+    CommandType.CALL_NEXT_LAYER_OBJECT,
   ],
   [
     CommandType.INCREMENT_STATE_COUNTER_VIA_AWAKEABLE,
@@ -53,12 +53,68 @@ const L0 = distribution([
 /**
  * L1 distribution represents the commands with their 'rank' (~ likelihood to appear)
  */
-const L1 = L0;
+const L1 = distribution([
+  [
+    CommandType.GET_STATE,
+    CommandType.SET_STATE,
+    CommandType.CLEAR_STATE,
+    CommandType.SIDE_EFFECT,
+    CommandType.INCREMENT_STATE_COUNTER,
+  ],
+  [
+    CommandType.INCREMENT_STATE_COUNTER_INDIRECTLY,
+    CommandType.CALL_SERVICE,
+    CommandType.CALL_NEXT_LAYER_OBJECT,
+  ],
+  [
+    CommandType.INCREMENT_STATE_COUNTER_VIA_AWAKEABLE,
+    CommandType.RESOLVE_AWAKEABLE,
+    CommandType.REJECT_AWAKEABLE,
+  ],
+  [
+    // CommandType.THROWING_SIDE_EFFECT, 
+    CommandType.RECOVER_TERMINAL_CALL],
+  [
+    // CommandType.CALL_SLOW_SERVICE,
+    CommandType.SLOW_SIDE_EFFECT,
+    // CommandType.SLEEP,
+    // CommandType.INCREMENT_VIA_DELAYED_CALL,
+    CommandType.RECOVER_TERMINAL_MAYBE_UN_AWAITED,
+  ],
+]);
 
 /**
  * L2 distribution represents the commands with their 'rank' (~ likelihood to appear)
  */
-const L2 = L0;
+const L2 = distribution([
+  [
+    CommandType.GET_STATE,
+    CommandType.SET_STATE,
+    CommandType.CLEAR_STATE,
+    CommandType.SIDE_EFFECT,
+    CommandType.INCREMENT_STATE_COUNTER,
+  ],
+  [
+    CommandType.INCREMENT_STATE_COUNTER_INDIRECTLY,
+    CommandType.CALL_SERVICE,
+    // CommandType.CALL_NEXT_LAYER_OBJECT,
+  ],
+  [
+    CommandType.INCREMENT_STATE_COUNTER_VIA_AWAKEABLE,
+    CommandType.RESOLVE_AWAKEABLE,
+    CommandType.REJECT_AWAKEABLE,
+  ],
+  [
+    // CommandType.THROWING_SIDE_EFFECT, 
+    CommandType.RECOVER_TERMINAL_CALL],
+  [
+    // CommandType.CALL_SLOW_SERVICE,
+    CommandType.SLOW_SIDE_EFFECT,
+    // CommandType.SLEEP,
+    // CommandType.INCREMENT_VIA_DELAYED_CALL,
+    CommandType.RECOVER_TERMINAL_MAYBE_UN_AWAITED,
+  ],
+]);
 
 const MAX_NUMBER_OF_LEVELS = 3;
 const DISTRIBUTION_BY_LEVEL = [L0, L1, L2];
