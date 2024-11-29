c081f5b482f34b626fe1c9e6778f4eee04f33869
igalshilman <igal@restate.dev>
2024-11-29 18:13:56 +0100

    add emojis

--- src/interpreter/test_driver.ts
+++ src/interpreter/test_driver.ts
@@ -156,7 +156,7 @@ export class Test {
       await sleep(2000);
     }
     console.log(`Admin is ready. ${adminUrl}`);
-    console.log(`RESTATE_ADMIN_URL=${adminUrl} ./target/debug/restate inv ls`);
+    console.log(`🌍 RESTATE_ADMIN_URL=${adminUrl} ./target/debug/restate inv ls`);
     for (const uri of deployments) {
       const res = await fetch(`${adminUrl}/deployments`, {
         method: "POST",
@@ -199,6 +199,9 @@ export class Test {
 
       console.log("Mapped ports of the restate leader");
       console.log(this.containers.container("n1").ports());
+
+      console.log(`🌍 RESTATE_CLUSTER_CONTROLLER_ADDRESS=http://localhost:${this.containers.container("n1").port(5122)} ./target/debug/restatectl status`);
+
     }
     let ingressUrls;
     if (this.containers) {
