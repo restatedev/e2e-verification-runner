2efff5cf20b3dc2686ff26fe7e20a8fca81a1c22
igalshilman <igal@restate.dev>
2025-02-24 16:22:29 +0100

    Use the correct image

--- scripts/run-verification.sh
+++ scripts/run-verification.sh
@@ -5,7 +5,7 @@
 #
 export DRIVER_IMAGE=${DRIVER_IMAGE:-"ghcr.io/restatedev/e2e-verification-runner:main"}
 export RESTATE_CONTAINER_IMAGE=${RESTATE_CONTAINER_IMAGE:-"ghcr.io/restatedev/restate:main"}
-export SERVICES_CONTAINER_IMAGE=${SERVICES_CONTAINER_IMAGE:-"ghcr.io/restatedev/node-test-services:main"}
+export SERVICES_CONTAINER_IMAGE=${SERVICES_CONTAINER_IMAGE:-"ghcr.io/restatedev/test-services-node:main"}
 export ENV_FILE=${ENV_FILE:-"correctness/env.json"}
 export PARAMS_FILE=${PARAMS_FILE:-"correctness/params.json"}
 
